8faceab3858da9a967f9a050338de263d0ae8d2f
西安站 <424447832@qq.com>
2021-11-13 15:57:11 +0800

    feature: 服务注册与发现 eureka 单机模式

---
+++ hubu-component-register/src/main/java/xianzhan/hubu/HubuEurekaServerApplication.java
@@ -0,0 +1,19 @@
+package xianzhan.hubu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册
+ *
+ * @author xianzhan
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class HubuEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubuEurekaServerApplication.class, args);
+    }
+}
