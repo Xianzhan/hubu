061969ec28e603a7104e1e2111ae0a54f2e48384
西安站 <424447832@qq.com>
2021-11-26 00:05:57 +0800

    move: 移动启动类

--- hubu-component-gateway/src/main/java/xianzhan/hubu/component/gateway/HubuGatewayApplication.java
+++ hubu-component-gateway/src/main/java/xianzhan/hubu/component/gateway/HubuGatewayApplication.java
@@ -1,4 +1,4 @@
-package xianzhan.hubu;
+package xianzhan.hubu.component.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hubu-component-register/src/main/java/xianzhan/hubu/component/register/HubuEurekaServerApplication.java
+++ hubu-component-register/src/main/java/xianzhan/hubu/component/register/HubuEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package xianzhan.hubu;
+package xianzhan.hubu.component.register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
